0abb4e7ba80ac7e09281bb0136e529a41124fa61
Grigoriy Chernish <cga.jsdev@gmail.com>
2022-08-03 18:31:12 +0300

    commit

--- arrayMetodTask.js
+++ arrayMetodTask.js
@@ -1,16 +1,13 @@
-// Task 1
-
+//TODO:==============================================
 // Получить массив имен всех пользователей (поле name).
 // console.log(getUserNames(usersData))
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
-// Task 2
-
+//TODO:==============================================
 // Получить массив объектов пользователей по цвету глаз (поле eyeColor).
 // console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
-// Task 3
-
+//TODO:==============================================
 // Получить массив имен пользователей по полу (поле gender)
 // console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
@@ -24,19 +21,16 @@
 // }
 // console.log(getUsersWithGender(users, 'male'))
 
-// Task 4
-
+//TODO:==============================================
 // Получить массив только неактивных пользователей (поле isActive).
 // console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
-// Task 5
-
+//TODO:==============================================
 // Получить пользователя (не массив) по email (поле email, он уникальный).
 // console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
 // console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
 
-// Task 6
-
+//TODO:==============================================
 // Получить массив пользователей в возрасте от min до max
 
 // console.log( getUsersWithAge( users, 20, 30 ) );
@@ -44,32 +38,27 @@
 // console.log( getUsersWithAge( users, 30, 40 ) );
 // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
-// Task 7
-
+//TODO:==============================================
 // Получить общую сумму баланса (поле balance) всех пользователей.
 // console.log(calculateTotalBalance(users)); // 20916
 
-// Task 8
-
+//TODO:==============================================
 // Массив имен всех пользователей у которых есть друг с указанным именем.
 // console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 // console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
-// Task 9
-
+//TODO:==============================================
 // Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
 // console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
-// Task 10
-
+//TODO:==============================================
 // Получить массив всех умений всех пользователей (поле skills), при этом не должно быть
 // повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 // console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
 
-// Task 11
-
+//TODO:==============================================
 // На входе массив чисел, например: arr = [1,2,3,4,5].
 // Напишите функцию getSums(arr), которая возвращает массив его частичных сумм.
 // Иначе говоря, вызов getSums(arr) должен возвращать новый массив из такого же числа элементов,
