5b93558fb16804c161901bda0592586547d08cea
Grigoriy Chernish <cga.jsdev@gmail.com>
2023-01-26 16:13:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 //TODO:=================01====================
-//Напишите две функции
-// letMeSeeYourName(callback) - спрашивает имя пользователя
-//через prompt и вызывает callback функцию
-//greet(name) - коллбек принимающий имя и логирующий в консоль
-//строку "Привет <name>"
-//Реализуй проверку, что prompt не пустой
-//++++++++++++++++++++++++++++++++++++++++++
-function letMeSeeYourName(callback) {
-  const name = prompt("What is your name?");
-  if (!name) return;
+//Напишіть дві функції
+// letMeSeeYourName(callback) - запитує ім'я користувача
+//через prompt та викликає callback функцію
+//greet(name) - коллбек, що приймає ім'я і логірує в консоль
+//Рядок "Привіт <name>"
+//Реалізуй перевірку, що prompt не порожній
+//++++++++++++++++++++++++++++++++++++++++++++
+// function letMeSeeYourName(callback) {
+// const name = prompt ("What is your name?");
+// if (! name) return;
 
-  return callback(name);
-}
-const greet = (name) => `Привет ${name}`;
+// return callback(name);
+// }
+// const greet = (name) => `Привіт ${name}`;
 // console.log(letMeSeeYourName(greet));
 
 //TODO:==============================================
-//Напишите две функции
-//makeProduct(name, price, callback) - принимает
-//имя и цену товара, а так же callback.
-//Функция создает объект товара, добавляя ему уникальный
-//идентификатор в свойство id и вызывает callback
-//передавая ему созданный объект.
-//showProduct(product) - коллбек принимающий объект
-//продукта и логирующий его в консоль
+//Напишіть дві функції
+//makeProduct(name, price, callback) - приймає
+//ім'я та ціну товару, а також callback.
+//Функція створює об'єкт товару, додаючи йому унікальний
+//ідентифікатор як id і викликає callback
+//Передаючи йому створений об'єкт.
+//showProduct(product) - коллбек приймаючий об'єкт
+//продукту і логірующий їх у консоль
 //++++++++++++++++++++++++++++++++++++++++++
 function makeProduct(name, price, callback) {
   const product = {
@@ -40,22 +40,22 @@ const showProduct = (product) => product;
 // console.table(makeProduct("Fish", 350, showProduct));
 
 //TODO:==============================================
-//Выполни рефакторинг makeDish так, чтобы не нужно было
-//каждый раз передавать имя шефа.
-//Напишите функцию makeShef(shefName) которая возвращает функцию
-//makeDish(dish), помнящую имя шефа при её вызове
+//Виконай рефакторинг makeDish так, щоб не потрібно було
+// Щоразу передавати ім'я шефа.
+//Напишіть функцію makeShef(shefName), яка повертає функцію
+//makeDish(dish), що пам'ятає ім'я шефа під час її виклику
 
 // const makeDish = function (shefName, dish) {
-//   console.log(`${shefName} is cooking ${dish}`);
-// };
+// console.log(`${shefName} is cooking ${dish}`);
+//};
 
 // makeDish("Mango", "apple pie");
 // makeDish("Poly", "muffins");
 //++++++++++++++++++++++++++++++++++++++++++
 const makeShef = function (name) {
   /*
-   * Параметр name это локальная переменная для функции makeShef.
-   * Это значит что она будет доступна функции makeDish через замыкание.
+   * Параметр name це локальна змінна для функції makeShef.
+   * Це означає, що вона буде доступна функції makeDish через замикання.
    */
   return function makeDish(dish) {
     console.log(`${name} is cooking ${dish}`);
@@ -71,7 +71,7 @@ const poly = makeShef("Poly");
 // poly("eggs and bacon"); // Poly is cooking eggs and bacon
 
 //TODO:==============================================
-//Исправьте ошибки, чтобы код работал
+//Виправте помилки, щоб код працював
 // const product = {
 //   price: 5000,
 //   showPrice() {
@@ -81,7 +81,7 @@ const poly = makeShef("Poly");
 // product.showPrice();
 
 //TODO:==============================================
-//Исправьте ошибки, чтобы код работал
+//Виправте помилки, щоб код працював
 const product = {
   price: 5000,
   showPrice() {
@@ -97,12 +97,12 @@ function callAction(action) {
 // callAction(product.showPrice.bind(product));
 
 //TODO:==============================================
-//Напишите функцию each(array, callback), которая
-//первым параметром принимает массив, а вторым - функцию,
-//которая применится к каждому элементу массива.
-//Функция each должна вернуть новый массив, элементами
-//которого будут результаты вызова callback
-//callback функци должна умножать элементы на 2
+//Напишіть функцію each(array, callback), яка
+//першим параметром приймає масив, а другим - функцію,
+//Яка застосовується до кожного елементу масиву.
+//Функція each повинна повернути новий масив, елементами
+//якого будуть результати виклику callback
+//callback функції повинна множити елементи на 2
 //++++++++++++++++++++++++++++++++++++++++++
 function each(array, callback) {
   const newArray = [];
@@ -116,8 +116,8 @@ const array = [3, 5, 6, 34, 8, 83, 12, 34];
 // console.log(each(array, (value) => value * 2));
 
 //TODO:==============================================
-//Напишите функцию makeCounter, которая возвращает другую
-//функцию, которая считает и логирует количество своих вызовов
+//Напишіть функцію makeCounter, яка повертає іншу
+//функцію, яка вважає та логує кількість своїх викликів
 //++++++++++++++++++++++++++++++++++++++++++
 function makeCounter() {
   let counter = 0;
@@ -130,10 +130,10 @@ const counter = makeCounter();
 // console.log(counter());
 
 //TODO:==============================================
-//Напишите функцию savePassword(password) которая принимает
-//пароль и возвращает внутреннюю функцию, которая принимает
-//строку и возвращает буль true, если строка совпадает с сохраненным
-//паролем и false - если не совпадает
+//Напишіть функцію savePassword(password), яка приймає
+//пароль і повертає внутрішню функцію, що приймає
+//рядок і повертає буль true, якщо рядок збігається зі збереженим
+//паролем і false - якщо не збігається
 //++++++++++++++++++++++++++++++++++++++++++
 const savePassword = (password) => {
   return (ourPassword) => password === ourPassword;
@@ -143,10 +143,9 @@ const standartPassword = savePassword("qwerty");
 // console.log(standartPassword("qwerty"));
 
 //TODO:==============================================
-//Напишите функцию для хранения скидки.Функция возвращает
-//другую функцию, которая принимает сумму покупки
-//и возвращает финальную сумму с сохраненной скидкой.
-
+//Напишіть функцію для зберігання знижки. Функція повертає
+//Іншу функцію, яка приймає суму покупки
+//і повертає фінальну суму із збереженою знижкою.
 //++++++++++++++++++++++++++++++++++++++++++
 // function saveDiscount(discount) {
 //   return function makeDiscount(sum) {
@@ -221,152 +220,3 @@ console.log(four(divide(two())));
 // console.log(one(plus(three()))); // 4
 // console.log(four(mult(three()))); // 12
 // console.log(four(divide(two())));
-
-const users = [
-  {
-    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    skills: ["ipsum", "lorem"],
-    gender: "male",
-    age: 37,
-  },
-  {
-    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-    gender: "female",
-    age: 34,
-  },
-  {
-    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-    gender: "male",
-    age: 24,
-  },
-  {
-    id: "249b6175-5c30-44c6-b154-f120923736f5",
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    skills: ["adipisicing", "irure", "velit"],
-    gender: "female",
-    age: 21,
-  },
-  {
-    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    skills: ["ex", "culpa", "nostrud"],
-    gender: "male",
-    age: 27,
-  },
-  {
-    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    skills: ["non", "amet", "ipsum"],
-    gender: "male",
-    age: 38,
-  },
-  {
-    id: "e1bf46ab-7168-491e-925e-f01e21394812",
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    skills: ["lorem", "veniam", "culpa"],
-    gender: "female",
-    age: 39,
-  },
-];
-
-// 7.1
-// Получить массив имен всех пользователей (поле name).
-
-// console.log(getUserNames(usersData))
-// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
-
-// 7.2
-// Получить массив объектов пользователей по цвету глаз (поле eyeColor).
-
-// console.log(getUsersByColor(users, 'brown'))
-// console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
-
-// 7.3
-// Получить массив имен пользователей по полу (поле gender)
-
-// console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
-
-// 7.4
-// Получить массив только неактивных пользователей (поле isActive).
-
-// console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
-
-// 7.5
-// Получить пользователя (не массив) по email (поле email, он уникальный).
-
-// console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
-// console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
-
-// 7.6
-// Получить массив пользователей в возрасте от min до max
-
-// console.log(getUsersWithAge(users, 30, 40))
-// [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
-// console.log( getUsersWithAge( users, 30, 40 ) );
-// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
-
-// 7.7
-// Получить общую сумму баланса (поле balance) всех пользователей.
-
-// console.log(calculateTotalBalance(users)); // 20916
-
-// 7.8
-// Массив имен всех пользователей у которых есть друг с указанным именем.
-
-// console.log(getUsersWithFriend(users, 'Briana Decker')) // [ 'Sharlene Bush', 'Sheree Anthony' ]
-// console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
-
-// 7.9
-// Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
-
-// console.log(getNamesSortedByFriendsCount(users));
-// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
-
-// 7.10
-// Получить массив всех умений всех пользователей (поле skills), при этом не должно быть
-// повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
-
-// console.log(getSortedUniqueSkills(users))
-// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
-
-//
